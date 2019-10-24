0a5026872560ce6864d135b4742bb8a3f51d0eb3
hunterdavisdev <hunterdavis.lua@gmail.com>
2019-10-24 00:20:33 -0500

    Routing fix

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
+
+  app.use('/nba', require('./routes/nba'));
 }
 
 app.listen(port, () => {
