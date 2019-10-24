fa7467be87d82db04bfbeb18389118b5e50756d9
hunterdavisdev <hunterdavis.lua@gmail.com>
2019-10-24 10:31:44 -0500

    oops

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use('/nba', require('./routes/nba'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('/client/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
